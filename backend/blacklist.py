a8ad9022d51c6e541e206671ed0473163763b36e
rahulsathish07 <213845870+rahulsathish07@users.noreply.github.com>
2025-12-03 13:13:25 +0000

    Updated blacklist.py (Change the path of text file after downloading and pushing to github)

---
+++ backend/blacklist.py
@@ -0,0 +1,34 @@
+import os
+g = os.path.join(
+    os.path.dirname(__file__),    # location of the blacklist text file, currently not created so change the path as per the file name when downloaded
+    "..",                       
+    "resources",                
+    "dontforgettochange.txt"   #change after downloading the textfile
+)
+
+# Store all blacklisted passwords here
+blacklisted = set()
+
+
+def load_blacklist():
+    """
+    Load the blacklist file into a Python set.
+    Runs only once, when the module is imported.
+    """
+    if not os.path.exists(g):
+        print("blacklist file not found", g)
+        return
+    
+    with open(g, "r") as f:
+        for line in f:
+            password = line.strip().lower()
+            if password:
+                blacklisted.add(password)
+
+load_blacklist() #function call
+
+
+def is_common_password(password: str) -> bool:
+    return password.lower() in blacklisted  #returns whether or not password is in the blacklisted set. Based on that it affects the total score of the password found by evaluator python file 
+
+
